bec39465a20b6638f8710b01360008ea3d993b8e
chaitra78 <53751398+chaitra78@users.noreply.github.com>
2025-09-16 14:41:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket        = "897722687643-web-tf-state-bucket"
+    bucket        = "my-s3-bucket-name-cr"
     key           = "usecase7/terraform.tfstate"
     region        = "us-east-1"
     encrypt       = true
